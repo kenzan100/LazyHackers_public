08292192d23af29403cd08e3a7a82a2405bba165
Okazaki Yuta <s04155yo@gmail.com>
2011-10-07 12:48:14 +0900

    smtp for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,20 @@ Lazyhackers4::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'lazyhackers.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => 'true',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'lazyhackers.com',
+    :authentication => :plain,
+    :user_name => 'info@lazyhackers.com',
+    :password => 'l0a9z0y3'
+  }
+
 end
