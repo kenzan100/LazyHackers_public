acfdbbec495c67ea46366228e8cc87652252cb9a
Okazaki Yuta <s04155yo@gmail.com>
2011-10-13 22:49:32 +0900

    for takaoka

--- db/schema_copy.rb
+++ db/schema_copy.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111005093820) do
+ActiveRecord::Schema.define(:version => 20111011093502) do
 
   create_table "hack_tag_follows", :force => true do |t|
     t.integer  "hack_tag_id"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(:version => 20111005093820) do
     t.integer "hack_tag_id"
   end
 
+  create_table "parties_scopes", :force => true do |t|
+    t.integer  "party_id"
+    t.integer  "scope_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "progres", :force => true do |t|
     t.boolean  "success"
     t.integer  "hack_tag_id"
@@ -59,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20111005093820) do
     t.boolean  "dropout"
     t.integer  "party_id"
     t.string   "comment"
+    t.boolean  "instruction_flag"
   end
 
   add_index "progres", ["hack_tag_id"], :name => "index_progres_on_hack_tag_id"
