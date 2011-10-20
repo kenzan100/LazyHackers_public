5f5d2ca67b2bf76f940388730439992f702e03d7
Okazaki Yuta <s04155yo@gmail.com>
2011-10-20 12:10:42 +0900

    last done on that hack_tag last scope

--- app/models/users_hacktag.rb
+++ app/models/users_hacktag.rb
@@ -4,14 +4,14 @@ class UsersHacktag < ActiveRecord::Base
   
   def self.update_from_progres(progres)
     progres.each do |progre|
-      unless UsersHacktag.exists?(:user_id=>progre.user_id, :hack_tag_id=>progre.hack_tag_id)
-        users_hacktag = UsersHacktag.new(:user_id=>progre.user_id, :hack_tag_id=>progre.hack_tag_id)
-        users_hacktag.save
-      end
-      
       if progre.dropout == true
         UsersHacktag.where(:user_id=>progre.user_id, :hack_tag_id=>progre.hack_tag_id).each do |uht|
           uht.destroy
+        end
+      else
+        unless UsersHacktag.exists?(:user_id=>progre.user_id, :hack_tag_id=>progre.hack_tag_id)
+          users_hacktag = UsersHacktag.new(:user_id=>progre.user_id, :hack_tag_id=>progre.hack_tag_id)
+          users_hacktag.save
         end
       end
     end
