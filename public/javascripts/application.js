c9f06dd2073cd1fa8c41982e52050971ad8348f7
Okazaki Yuta <s04155yo@gmail.com>
2011-10-21 16:43:34 +0900

    pageshow jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,11 +11,21 @@ $('#Scope').live("pagecreate", function(){
 	$('#TellUsYourKnowhow').click(function(){
   	$('#add_comment').show("slow");
   	$('#TellUsYourKnowhow').hide();
-	});
+  	
+});
 
 $('#Scope').live("pageshow", function(){
   $('span.ui-btn-text').css('z-index', '0');
   });
+	
+	$('#AddHackTag').change(function(){
+	  if($(this).children('option:selected').text() == '新規作成'){
+	    $('#WantingNewTag').show();
+	    $('form#new_users_scope').hide();
+	  }else{
+		  $(this).parents('form#AddingHackTag').submit();
+	  }
+	});
 	
 });
 
@@ -26,16 +36,6 @@ $(function(){
 		$('#DefiningScope form').toggle();
 		$('#Header h1').toggle();
 	});
-	
-	$('#AddHackTag').change(function(){
-	  if($(this).children('option:selected').text() == '新規作成'){
-	    $('#WantingNewTag').show();
-	    $('form#new_users_scope').hide();
-	  }else{
-		  $(this).parents('form#AddingHackTag').submit();
-	  }
-	});
-
 
 //	$('form.GoWatchThereForm').find('span.ui-btn-text').live('click', function(){
 //	  $(this).parents('form.GoWatchThereForm').submit();
